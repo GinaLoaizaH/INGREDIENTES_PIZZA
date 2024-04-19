fff4482c3a212bbe4359eeec68f57337cbdb7e16
GinaLoaizaH <ginita011295@gmail.com>
2024-04-18 20:46:58 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Ingredientes Pizza");
+
+        // 3 ingredientes por participante = GINA
+        String ingrediente1 = "Peperoni";
+        String ingrediente2 = "Jamón";
+        String ingrediente3 = "Pimentón";
+
+        // 3 ingredientes por participante =
+    }
+}
